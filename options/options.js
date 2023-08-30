e77121b5c4e839098a79bdebb5afb7b1480a6855
Crowdbotics <engineering@crowdbotics.com>
2023-08-30 23:48:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-wildflower-d-113022",
+  url: "https://royal-wildflower-d-113022.botics.co",
+  api: "https://royal-wildflower-d-113022.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
